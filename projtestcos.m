2dac3bff54af146d6a140c14bc7cc25617e25f04
Logan-S-Sch <logan4him@gmail.com>
2025-12-03 10:59:25 -0800

    Add files via upload

--- projtestcos.m
+++ projtestcos.m
@@ -8,7 +8,8 @@ p_Wolf = polyfit(year, Wolf, 3);
 % Generate smooth x-axis
 year_fit = linspace(1985, 2000, 200);
 
-
+% This section Creates variables that fine tune the Sin formula and creates
+% the graph for wolves
 Amp_W = 2.1;
 Omega_W = 1.2;
 phi_W = pi / 2;
@@ -16,17 +17,19 @@ initial_val_W = 22;
 x = (year_fit - 1985);
 Wolf_fit = Amp_W*sin(Omega_W* x + phi_W) - (20/33) * x + initial_val_W;
 
-
+% This section Creates variables that fine tune the Sin formula for The MSE
+% Function
 Amp_W = 2.1;
 Omega_W = 1.2;
 phi_W = pi / 2;
 initial_val_W = 22;
 
-% Evaluate model at actual year points:
+% Year data points for the MSE function
 x_data = year - 1985;
 Wolf_model_at_data = Amp_W .* sin(Omega_W .* x_data + phi_W) - (20/33).*x_data + initial_val_W;
 
-
+% This section Creates variables that fine tune the Cos formula for The MSE
+% Function for moose
 Amp_M = -390;
 initial_val_M = 1300;
 x_data = year - 1985;
@@ -40,7 +43,7 @@ initial_val_M = 1300;
 x = (year_fit - 1985);   % maps 1985→0, 2000→π
 Moose_fit = Amp_M .* cos(x) + initial_val_M;
 
-%% Matrics
+%% MSE function
 MSE_W = immse(Wolf , Wolf_model_at_data)
 MSE_M = immse(Moose , Moose_model_at_data)
 
@@ -50,7 +53,7 @@ disp(['Moose MSE = ', num2str(MSE_M)]);
 % --- Plot results ---
 figure;
 
-% Wolves (polynomial)
+% Wolves (Sin Model)
 subplot(2,1,1);
 plot(year, Wolf, 'ro', 'MarkerFaceColor', 'r'); hold on;
 plot(year_fit, Wolf_fit, 'r-');
